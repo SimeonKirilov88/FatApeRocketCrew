8339a7269854eae67844552eb67ede36ede9f651
Shop Online <simeonkirilov88@gmail.com>
2021-09-08 18:58:25 +0200

    Create FatApe.sol

---
+++ FatApe.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/**
+ *
+ *               
+ * 
+ *    /$$$$$$$$         /$$            /$$$$$$                            /$$$$$$$                      /$$                   /$$            /$$$$$$                                   
+ *   | $$_____/        | $$           /$$__  $$                          | $$__  $$                    | $$                  | $$           /$$__  $$                                  
+ *   | $$    /$$$$$$  /$$$$$$        | $$  \ $$  /$$$$$$   /$$$$$$       | $$  \ $$  /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$  /$$$$$$        | $$  \__/  /$$$$$$   /$$$$$$  /$$  /$$  /$$
+ *   | $$$$$|____  $$|_  $$_/        | $$$$$$$$ /$$__  $$ /$$__  $$      | $$$$$$$/ /$$__  $$ /$$_____/| $$  /$$/ /$$__  $$|_  $$_/        | $$       /$$__  $$ /$$__  $$| $$ | $$ | $$
+ *   | $$__/ /$$$$$$$  | $$          | $$__  $$| $$  \ $$| $$$$$$$$      | $$__  $$| $$  \ $$| $$      | $$$$$$/ | $$$$$$$$  | $$          | $$      | $$  \__/| $$$$$$$$| $$ | $$ | $$
+ *   | $$   /$$__  $$  | $$ /$$      | $$  | $$| $$  | $$| $$_____/      | $$  \ $$| $$  | $$| $$      | $$_  $$ | $$_____/  | $$ /$$      | $$    $$| $$      | $$_____/| $$ | $$ | $$
+ *   | $$  |  $$$$$$$  |  $$$$/      | $$  | $$| $$$$$$$/|  $$$$$$$      | $$  | $$|  $$$$$$/|  $$$$$$$| $$ \  $$|  $$$$$$$  |  $$$$/      |  $$$$$$/| $$      |  $$$$$$$|  $$$$$/$$$$/
+ *   |__/   \_______/   \___/        |__/  |__/| $$____/  \_______/      |__/  |__/ \______/  \_______/|__/  \__/ \_______/   \___/         \______/ |__/       \_______/ \_____/\___/ 
+ *                                             | $$                                                                                                                                    
+ *                                             | $$                                                                                                                                    
+ *                                                                                                                                                                            
+ *                                                                                                                                                                                                                                                                                                                                                                                                                        
+ * 10,000 Fat Apes are flying through the Metaverse.
+ * 
+ * Become a Fat Ape Crew and onboard the Rocket 
+ * 
+ * Next Destination -> To The Moon 
+ * 
+ * Let's Pump it Up 
+ * 
+ *  More on https://fataperocketcrew.com
+ * 
+*/
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FatApeRocketCrew is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  string public baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.000 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmount = 5000;
+  bool public paused = false;
+  mapping(address => bool) public whitelisted;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI
+  ) ERC721(_name, _symbol) {
+     
+    setBaseURI(_initBaseURI);
+   
+    mint(msg.sender, 200);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  // public
+  function mint(address _to, uint256 _mintAmount) public payable {
+    uint256 supply = totalSupply();
+    require(!paused);
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
+
+    if (msg.sender != owner()) {
+        if(whitelisted[msg.sender] != true) {
+          require(msg.value >= cost * _mintAmount);
+        }
+    }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(_to, supply + i);
+    }
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+  //only owner
+  function setCost(uint256 _newCost) public onlyOwner() {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
+    maxMintAmount = _newmaxMintAmount;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+ 
+ function whitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = true;
+  }
+ 
+  function removeWhitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = false;
+  }
+
+  function withdraw() public payable onlyOwner {
+    require(payable(msg.sender).send(address(this).balance));
+  }
+}
